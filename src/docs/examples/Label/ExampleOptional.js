96ad3575b20ea6f259dfc2bd9e07f5d504569a27
Emilio <em.francomartinez@gmail.com>
2020-05-11 16:11:26 -0600

    eject react-app, set-up documentation, create some atom components with examples

---
+++ src/docs/examples/Label/ExampleOptional.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Label from "ps-react/Label";
+
+/**Optional Label */
+const ExampleOptional = () => {
+  return <Label htmlFor="test" label="test" />;
+};
+
+export default ExampleOptional;
